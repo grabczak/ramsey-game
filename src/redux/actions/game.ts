1a02239bef9b24f04c8bfe75a562ec654b9dd04b
Kacper Grabczak <kacpergrabczak@gmail.com>
2020-05-10 22:49:33 +0200

    K3 strategy

--- src/redux/actions/game.ts
+++ src/redux/actions/game.ts
@@ -5,7 +5,7 @@ import {
   END_GAME,
   NEXT_MOVE,
 } from '../../constants/actions';
-import { TNode, TEdge } from '../../typings/state';
+import { TNode, TEdge, TRootState } from '../../typings/state';
 import { wait } from '../../utils/wait';
 
 const jsnx = require('jsnetworkx');
@@ -47,7 +47,8 @@ export const nextMove = (edge: TEdge) => {
 };
 
 export const playerMove = (source: number, target: number) => {
-  return async (dispatch: any, getState: any) => {
+  return async (dispatch: any, getState: () => TRootState) => {
+    const graphSize = getState().game.nodes.length;
     const { targetCliqueSize } = getState().game;
 
     dispatch(nextMove({ source, target, team: 'player' }));
@@ -73,72 +74,124 @@ export const playerMove = (source: number, target: number) => {
       return;
     }
 
-    const findNewEdge = (): Promise<TEdge & { winningEdge: boolean }> => {
-      return new Promise((resolve) => {
-        for (const a of jsnxNodes) {
-          for (const b of jsnxNodes) {
-            if (a >= b) {
-              continue;
-            }
+    const computerGraph = new jsnx.Graph();
+    computerGraph.addNodesFrom(jsnxNodes);
+    computerGraph.addEdgesFrom(jsnxComputerEdges);
 
-            const newComputerGraph = new jsnx.Graph();
-            newComputerGraph.addNodesFrom(jsnxNodes);
-            newComputerGraph.addEdgesFrom(jsnxComputerEdges);
-            newComputerGraph.addEdge(a, b);
+    const findNewEdge = async (): Promise<TEdge & { winningEdge: boolean }> => {
+      if (jsnxPlayerEdges.length === 1) {
+        const [v1, v2] = jsnxPlayerEdges[0];
 
-            if (
-              jsnx.graphCliqueNumber(newComputerGraph) === targetCliqueSize &&
-              possibleEdges.some(
-                (edge: TEdge) => edge.source === a && edge.target === b,
-              )
-            ) {
-              const newEdge = {
-                source: a,
-                target: b,
-                team: 'computer' as const,
-                winningEdge: true,
-              };
-
-              resolve(newEdge);
-            }
+        const v1Edges = [];
+        for (let i = 0; i < graphSize; i++) {
+          if (i !== v1 && i !== v2) {
+            v1Edges.push([v1, i]);
           }
         }
 
-        for (const a of jsnxNodes) {
-          for (const b of jsnxNodes) {
-            if (a >= b) {
-              continue;
-            }
+        const edge = v1Edges[Math.floor(Math.random() * v1Edges.length)];
 
-            const newPlayerGraph = new jsnx.Graph();
-            newPlayerGraph.addNodesFrom(jsnxNodes);
-            newPlayerGraph.addEdgesFrom(jsnxPlayerEdges);
-            newPlayerGraph.addEdge(a, b);
-
-            if (
-              jsnx.graphCliqueNumber(newPlayerGraph) === targetCliqueSize &&
-              possibleEdges.some(
-                (edge: TEdge) => edge.source === a && edge.target === b,
-              )
-            ) {
-              const newEdge = {
-                source: a,
-                target: b,
-                team: 'computer' as const,
-                winningEdge: false,
-              };
-
-              resolve(newEdge);
-            }
-          }
-        }
-
-        resolve({
-          ...possibleEdges[Math.floor(Math.random() * possibleEdges.length)],
+        return {
+          source: edge[0],
+          target: edge[1],
           team: 'computer',
           winningEdge: false,
-        });
-      });
+        };
+      }
+
+      for (const a of jsnxNodes) {
+        for (const b of jsnxNodes) {
+          if (a >= b) {
+            continue;
+          }
+
+          const newComputerGraph = new jsnx.Graph();
+          newComputerGraph.addNodesFrom(jsnxNodes);
+          newComputerGraph.addEdgesFrom(jsnxComputerEdges);
+          newComputerGraph.addEdge(a, b);
+
+          if (
+            possibleEdges.some(
+              (edge: TEdge) => edge.source === a && edge.target === b,
+            ) &&
+            jsnx.graphCliqueNumber(newComputerGraph) === targetCliqueSize
+          ) {
+            const newEdge = {
+              source: a,
+              target: b,
+              team: 'computer' as const,
+              winningEdge: true,
+            };
+
+            return newEdge;
+          }
+        }
+      }
+
+      for (const a of jsnxNodes) {
+        for (const b of jsnxNodes) {
+          if (a >= b) {
+            continue;
+          }
+
+          const newPlayerGraph = new jsnx.Graph();
+          newPlayerGraph.addNodesFrom(jsnxNodes);
+          newPlayerGraph.addEdgesFrom(jsnxPlayerEdges);
+          newPlayerGraph.addEdge(a, b);
+
+          if (
+            jsnx.graphCliqueNumber(newPlayerGraph) === targetCliqueSize &&
+            possibleEdges.some(
+              (edge: TEdge) => edge.source === a && edge.target === b,
+            )
+          ) {
+            const newEdge = {
+              source: a,
+              target: b,
+              team: 'computer' as const,
+              winningEdge: false,
+            };
+
+            return newEdge;
+          }
+        }
+      }
+
+      if (targetCliqueSize === 3) {
+        const playerDegrees = Array.from(jsnx.degree(playerGraph)).sort(
+          (a: any, b: any) => a[1] - b[1],
+        );
+
+        const computerDegrees = Array.from(jsnx.degree(computerGraph)).sort(
+          (a: any, b: any) => b[1] - a[1],
+        );
+
+        for (const computerDegree of computerDegrees) {
+          for (const playerDegree of playerDegrees) {
+            // @ts-ignore
+            const [v1, v2] = [computerDegree[0], playerDegree[0]].sort();
+
+            if (
+              possibleEdges.some(
+                (edge: TEdge) => edge.source === v1 && edge.target === v2,
+              )
+            ) {
+              return {
+                source: v1,
+                target: v2,
+                team: 'computer',
+                winningEdge: false,
+              };
+            }
+          }
+        }
+      }
+
+      return {
+        ...possibleEdges[Math.floor(Math.random() * possibleEdges.length)],
+        team: 'computer',
+        winningEdge: false,
+      };
     };
 
     const newEdge = await findNewEdge();
